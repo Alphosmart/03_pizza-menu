8900d84c8bafda3d76bc2c8bf2f01ace0b84c1de
Alphonsus Gabriel <alpho4luv@gmail.com>
2024-05-28 18:03:45 +0100

    Correction

--- .history/src/index_20240526124855.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom/client";
-
-function App() {Appreturn <h1>Hello React!</h1>}
--- .history/src/index_20240526125124.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom/client";
-
-function App() {
-  <h1>Hello React!</h1>;
-}
-
-const root = reactDOM.createRoot(document.getElementById);
