6d66a36260febebc8bd4724dc50b9078f2ea8871
Alphonsus Gabriel <84717082+Alphosmart@users.noreply.github.com>
2024-06-24 16:35:09 +0100

    t

--- .history/src/index_20240624163422.js
+++ .history/src/index_20240624163422.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
-    name: 'Focaccia',
-    ingredients: 'Bread with italian olive oil and rosemary',
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: 'pizzas/focaccia.jpg',
+    photoName: "pizzas/focaccia.jpg",
     soldOut: false,
   },
   {
-    name: 'Pizza Margherita',
-    ingredients: 'Tomato and mozarella',
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: 'pizzas/margherita.jpg',
+    photoName: "pizzas/margherita.jpg",
     soldOut: false,
   },
   {
-    name: 'Pizza Spinaci',
-    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: 'pizzas/spinaci.jpg',
+    photoName: "pizzas/spinaci.jpg",
     soldOut: false,
   },
   {
-    name: 'Pizza Funghi',
-    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: 'pizzas/funghi.jpg',
+    photoName: "pizzas/funghi.jpg",
     soldOut: false,
   },
   {
-    name: 'Pizza Salamino',
-    ingredients: 'Tomato, mozarella, and pepperoni',
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: 'pizzas/salamino.jpg',
+    photoName: "pizzas/salamino.jpg",
     soldOut: true,
   },
   {
-    name: 'Pizza Prosciutto',
-    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: 'pizzas/prosciutto.jpg',
+    photoName: "pizzas/prosciutto.jpg",
     soldOut: false,
   },
 ];
@@ -116,7 +116,7 @@ function Pizza({ pizzaObj }) {
   // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
@@ -128,7 +128,7 @@ function Pizza({ pizzaObj }) {
           <span>{pizzaObj.price}</span>
         )} */}
 
-        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -174,7 +174,7 @@ function Order({ closeHour, openHour }) {
 }
 
 // React v18
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
